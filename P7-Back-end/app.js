7197f375f2e2f7b655057d949abcd7326b293fd1
Elliot <clemenceauelliot@gmail.com>
2023-12-27 13:25:59 +0100

    all routes

---
+++ P7-Back-end/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose'); // Plugin Mongoose pour se connecter à la data base Mongo Db
+
+mongoose.connect("mongodb+srv://clemenceauelliot2:Th5MYaEFWnu2Z8g1@cluster0.fk5awyr.mongodb.net/?retryWrites=true&w=majority", {
+
+})
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch((error) => console.log(error));
+
+
+// const uri = "mongodb+srv://clemenceauelliot2:Th5MYaEFWnu2Z8g1@cluster0.fk5awyr.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// });
+
+var bodyParser = require('body-parser')
+
+const bookRoutes = require('./routes/book.js');
+const routerUser = require('./routes/user.js');
+
+
+const path = require('path');
+
+
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+
+
+app.use(bodyParser.json());
+app.use('/api/books', bookRoutes);
+app.use('/api/auth', routerUser);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
---
+++ P7-Back-end/models/book.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+    genre: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+    ratings: [
+        {
+            userId: {
+                type: String,
+                required: true,
+            },
+            grade: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+    averageRating: {
+        type: Number,
+        default: 4,
+    },
+});
+
+
+module.exports = mongoose.model('Book', bookSchema);
